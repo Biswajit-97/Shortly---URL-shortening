212e0b0e8af076648c4cd641f6f867db4a454fb5
Biswajit-97 <jeetmondal42@gmail.com>
2022-01-08 01:19:07 +0530

    Frontend Mentor - Url Shortening Landing Challange

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Shortlink from "./components/Shortlink";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Hero />
+     <Shortlink />
+     <Feature />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
